350bde96387f0820dbaa087ca9ee4332c218ec06
IslamAhmed1092 <eslam1092@hotmail.com>
2021-01-01 18:29:58 +0200

    organize folders and files

--- Preprocessing/commonfunctions.py
+++ Preprocessing/commonfunctions.py
@@ -1,26 +1,28 @@
+import numpy as np
 import skimage.io as io
 import matplotlib.pyplot as plt
-import numpy as np
+from skimage.morphology import binary_erosion, binary_dilation, binary_closing,skeletonize, thin
+from skimage.measure import find_contours
+from skimage.draw import rectangle
+from collections import Counter
+import skimage.filters as fr
+import skimage as sk
+import cv2, time, os, math
+from skimage.transform import hough_line, hough_line_peaks, rotate
+from rotation import rotateImage
+from remove_lines import getStaffLines
 from skimage.exposure import histogram
 from matplotlib.pyplot import bar
 from skimage.color import rgb2gray,rgb2hsv
-
 # Convolution:
 from scipy.signal import convolve2d
 from scipy import fftpack
-import math
-
 from skimage.util import random_noise
 from skimage.filters import median
 from skimage.feature import canny
-
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
-import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import numpy as np
-
 # Edges
 from skimage.filters import sobel_h, sobel, sobel_v,roberts, prewitt
 
@@ -43,44 +45,6 @@ def show_images(images,titles=None):
         n += 1
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
     plt.show() 
-    
-
-def show_3d_image(img, title):
-    fig = plt.figure()
-    fig.set_size_inches((12,8))
-    ax = fig.gca(projection='3d')
-
-    # Make data.
-    X = np.arange(0, img.shape[0], 1)
-    Y = np.arange(0, img.shape[1], 1)
-    X, Y = np.meshgrid(X, Y)
-    Z = img[X,Y]
-
-    # Plot the surface.
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                           linewidth=0, antialiased=False)
-
-    # Customize the z axis.
-    ax.set_zlim(0, 8)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-    # Add a color bar which maps values to colors.
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-    ax.set_title(title)
-    plt.show()
-    
-def show_3d_image_filtering_in_freq(img, f):
-    img_in_freq = fftpack.fft2(img)
-    filter_in_freq = fftpack.fft2(f, img.shape)
-    filtered_img_in_freq = np.multiply(img_in_freq, filter_in_freq)
-    
-    img_in_freq = fftpack.fftshift(np.log(np.abs(img_in_freq)+1))
-    filtered_img_in_freq = fftpack.fftshift(np.log(np.abs(filtered_img_in_freq)+1))
-    
-    show_3d_image(img_in_freq, 'Original Image')
-    show_3d_image(filtered_img_in_freq, 'Filtered Image')
-
 
 def showHist(img):
     # An "interface" to matplotlib.axes.Axes.hist() method
@@ -88,3 +52,79 @@ def showHist(img):
     imgHist = histogram(img, nbins=256)
     
     bar(imgHist[1].astype(np.uint8), imgHist[0], width=0.8, align='center')
+
+def binraization(img,n=8,t=15):
+
+    outputimg = np.zeros(img.shape)
+    intimg = np.zeros(img.shape)
+    h = img.shape[1]
+    w = img.shape[0]
+    s= min(w,h)//n
+    count = s**2
+    img = np.pad(img,s,"constant")
+    intimg = np.cumsum(img ,axis =1)
+    intimg = np.cumsum(intimg ,axis =0)
+    a = np.roll(intimg,-s//2,axis =0)
+    a = np.roll(a,-s//2,axis =1)
+    a[:,-s//2:]=a[-s//2-1,-s//2-1]
+    a[-s//2:,:]=a[-s//2-1,-s//2-1]
+    b = np.roll(intimg,s//2+1,axis =0)
+    b = np.roll(b,-s//2,axis =1)
+    b[0:s//2+1,:]=0
+    b[:,-s//2:]=0
+    
+    c = np.roll(intimg,s//2+1,axis =1)
+    c = np.roll(c,-s//2,axis =0)
+    c[:,0:s//2+1]=0
+    c[-s//2:,:]=0
+    
+    d = np.roll(intimg,s//2+1,axis =0)
+    d = np.roll(d,s//2+1,axis =1)
+    d[0:s//2+1,:]=0
+    d[:,0:s//2+1]=0
+
+    sum = (a-b-c+d)*(100-t)/100
+    outputimg = (img>sum/count)*255
+    return outputimg[s:-s,s:-s]
+
+
+
+def getRefLengths(img):
+    cols = img.shape[1]
+    rows = img.shape[0]
+    hist = np.zeros((rows,rows), dtype=np.uint32)
+    
+    for i in range(0, cols):
+        a = img[:,i]
+        starts = np.array((a[:-1] == 1) & (a[1:] == 0))
+        starts_ix = np.where(starts)[0] + 2
+        ends = np.array((a[:-1] == 0) & (a[1:] == 1))
+        ends_ix = np.where(ends)[0] + 2
+        s1 = starts_ix.size
+        s2 = ends_ix.size
+        if s2 > s1:
+            starts_ix = np.pad(starts_ix,(s2-s1,0), mode='constant', constant_values=(1))
+        elif s1 > s2:
+            ends_ix = np.pad(ends_ix,(0,s1-s2), mode='constant', constant_values=(a.size + 1))
+        elif s1 > 0 and s2 > 0 and starts_ix[0] > ends_ix[0]:
+            starts_ix = np.pad(starts_ix,(1,0), mode='constant', constant_values=(1))
+            ends_ix = np.pad(ends_ix,(0,1), mode='constant', constant_values=(a.size + 1))
+            
+        l0 = ends_ix - starts_ix
+        starts_ix1 = np.pad(starts_ix[1:],(0,1), mode='constant', constant_values=(a.size + 1)) 
+        l1 = starts_ix1 - (starts_ix + l0)
+        for i in range(s1):
+            hist[l0[i], l1[i]] += 1
+       
+    hist[:,0] = 0
+    mx = np.max(hist)
+    ind = np.where(hist == mx)
+    return ind[0][0], ind[1][0]
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename), 0)
+        if img is not None:
+            images.append(img)
+    return images
